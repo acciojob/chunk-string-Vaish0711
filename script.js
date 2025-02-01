3361a8fa1da51ec7b6c04fa454507b0cf6eee188
Vaish0711 <vaishnavidahane2000@gmail.com>
2025-02-01 23:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	if (str === null) return []; // Handle null input
+
+    let result = [];
+    for (let i = 0; i < str.length; i += size) {
+        result.push(str.slice(i, i + size));
+    }
+    
+    return result;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
